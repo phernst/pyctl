e0640fea975a2dbdd8a36a049991168b7c76efd6
Philipp Ernst <phil23940@yahoo.de>
2020-04-07 14:16:12 +0200

    remove unnecessary module variables

--- ctl/__init__.py
+++ ctl/__init__.py
@@ -1,24 +1,31 @@
-import os
-
 try:
     from importlib.resources import path
     cl_path = path('ctl', 'cl_src')
     db_path = path('ctl', 'database')
+    del path
 except ImportError:
     from importlib_resources import files
     cl_path = files('ctl') / 'cl_src'
     db_path = files('ctl') / 'database'
+    del files
 import atexit
 from contextlib import ExitStack
 import _ctl
 
-file_manager = ExitStack()
-atexit.register(file_manager.close)
-cl_source_dir = file_manager.enter_context(cl_path)
-db_source_dir = file_manager.enter_context(db_path)
+_file_manager = ExitStack()
+atexit.register(_file_manager.close)
+cl_source_dir = _file_manager.enter_context(cl_path)
+db_source_dir = _file_manager.enter_context(db_path)
+
+del ExitStack
+del atexit
+del cl_path
+del db_path
 
 _ctl.ocl.ClFileLoader.set_opencl_source_dir(str(cl_source_dir.resolve()))
 _ctl.CTLDatabaseHandler.set_database_root(str(db_source_dir.resolve()))
+del cl_source_dir
+del db_source_dir
 
 __all__ = [name for name in dir(_ctl) if name[0] != '_']
 
@@ -26,3 +33,5 @@ for name in dir(_ctl):
     if name[0] == '_':
         continue
     globals()[name] = getattr(_ctl, name)
+
+del _ctl
