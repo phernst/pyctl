c5896b7c772d0b711f410c6f3d2588bf35fb5d5f
Philipp Ernst <phil23940@yahoo.de>
2020-05-11 15:31:38 +0200

    update OCL::RayCasterProjector

--- pysrc/projectors/raycasterprojector.cpp
+++ pysrc/projectors/raycasterprojector.cpp
@@ -13,10 +13,6 @@ void init_raycasterprojector(py::module& m)
         .def(py::init<>())
         .def("configure", &OCL::RayCasterProjector::configure, "setup"_a, py::keep_alive<1,2>())
         .def("project", &OCL::RayCasterProjector::project, "volume"_a)
-        .def("project", [](OCL::RayCasterProjector* self, const VoxelVolume<float>& volume)
-        {
-            return self->project(volume);
-        }, "volume"_a)
         .def("settings", &OCL::RayCasterProjector::settings);
 
     py::class_<OCL::RayCasterProjector::Settings>(rcp, "Settings")
@@ -24,16 +20,16 @@ void init_raycasterprojector(py::module& m)
         .def_static("optimized_for", &OCL::RayCasterProjector::Settings::optimizedFor,
             "volume"_a, "detector"_a)
         .def_readwrite("device_ids", &OCL::RayCasterProjector::Settings::deviceIDs)
-        .def_property("rays_per_pixel", [](const OCL::RayCasterProjector::Settings* self)
+        .def_property("rays_per_pixel", [](const OCL::RayCasterProjector::Settings& self)
         {
             auto t { py::tuple(2) };
-            t[0] = self->raysPerPixel[0];
-            t[1] = self->raysPerPixel[1];
+            t[0] = self.raysPerPixel[0];
+            t[1] = self.raysPerPixel[1];
             return t;
-        }, [](OCL::RayCasterProjector::Settings* self, py::tuple t)
+        }, [](OCL::RayCasterProjector::Settings& self, py::tuple t)
         {
-            self->raysPerPixel[0] = t[0].cast<uint>();
-            self->raysPerPixel[1] = t[1].cast<uint>();
+            self.raysPerPixel[0] = t[0].cast<uint>();
+            self.raysPerPixel[1] = t[1].cast<uint>();
         })
         .def_readwrite("ray_sampling", &OCL::RayCasterProjector::Settings::raySampling)
         .def_readwrite("volume_upsampling", &OCL::RayCasterProjector::Settings::volumeUpSampling)
