b46530c009b7ca6a3ad7985cfdac3e8a4446c4ee
Philipp Ernst <philipp.ernst@ovgu.de>
2022-08-30 09:45:00 +0200

    update

--- pysrc/recon/simplebackprojector.cpp
+++ pysrc/recon/simplebackprojector.cpp
@@ -1,8 +1,32 @@
 #include "pysrc/pybind_common.h"
 #include <recon/simplebackprojector.h>
+#include <img/voxelvolume.h>
 #include "../pysharedptr.h"
 
 namespace py = pybind11;
+
+namespace
+{
+using namespace CTL;
+
+class PySimpleBackprojector : public OCL::SimpleBackprojector
+{
+public:
+    using OCL::SimpleBackprojector::SimpleBackprojector;
+    bool reconstructToPlain(const ProjectionDataView& projections,
+                            VoxelVolume<float>& targetVolume) override
+    {
+        PYBIND11_OVERRIDE_NAME(
+            bool,
+            OCL::SimpleBackprojector,
+            "reconstruct_to_plain",
+            reconstructToPlain,
+            projections,
+            targetVolume
+        );
+    }
+};
+}
 
 void init_simplebackprojector(py::module& m)
 {
@@ -11,7 +35,7 @@ void init_simplebackprojector(py::module& m)
     using SB_ = OCL::SimpleBackprojector;
     using BB_ = OCL::BackprojectorBase;
 
-    auto sbp = py::class_<SB_, BB_, PySharedPtr<SB_>>(m, "SimpleBackprojector");
+    auto sbp = py::class_<SB_, BB_, PySharedPtr<SB_>, PySimpleBackprojector>(m, "SimpleBackprojector");
 
     sbp.def(py::init<BB_::WeightingType>(), "weight_type"_a = BB_::WeightingType::GeometryFactors)
         .def("set_custom_weights", &SB_::setCustomWeights, "weights"_a)
